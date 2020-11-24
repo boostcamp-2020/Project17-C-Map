47c425932ca4d25224b133ca10c02943df5e2c84
Sungmin Park <rnfxl92@naver.com>
2020-11-24 14:55:10 +0900

    [feat] QuadTree에 사용할 BoundingBox 구현
    - 각 함수 구현
    - Cluster, Coordinate 구조체는 현재 mockup 구조체임

---
+++ Project17-C-Map/InteractiveClusteringMap/QuadTree/BoundingBox.swift
@@ -0,0 +1,58 @@
+//
+//  BoundingBox.swift
+//  InteractiveClusteringMap
+//
+//  Created by 박성민 on 2020/11/24.
+//
+
+import Foundation
+
+struct BoundingBox {
+    
+    let topRight: Coordinate
+    let bottomLeft: Coordinate
+    
+    var splitedQuadBoundingBox: [BoundingBox] {
+        [
+            BoundingBox(topRight: Coordinate(x: mid.x, y: topRight.y),
+                        bottomLeft: Coordinate(x: bottomLeft.x, y: mid.y)),
+            BoundingBox(topRight: topRight, bottomLeft: mid),
+            BoundingBox(topRight: mid, bottomLeft: bottomLeft),
+            BoundingBox(topRight: Coordinate(x: topRight.x, y: mid.y),
+                        bottomLeft: Coordinate(x: mid.x, y: bottomLeft.y))
+        ]
+    }
+    
+    private var mid: Coordinate {
+        let midX: Double = (bottomLeft.x + topRight.x) / 2.0
+        let midY: Double = (bottomLeft.y + topRight.y) / 2.0
+        return Coordinate(x: midX, y: midY)
+    }
+    
+    func contains(coordinate: Coordinate) -> Bool {
+        let containsX: Bool = (bottomLeft.x <= coordinate.x) && (coordinate.x <= topRight.x)
+        let containsY: Bool = (bottomLeft.y <= coordinate.y) && (coordinate.y <= topRight.y)
+        return (containsX && containsY)
+    }
+    
+    func intersectsBoxBounds(with box: BoundingBox) -> Bool {
+        return (bottomLeft <= box.topRight &&  box.bottomLeft <= topRight)
+    }
+    
+}
+
+struct Coordinate {
+    
+    let x: Double
+    let y: Double
+    
+    static func <= (left: Coordinate, right: Coordinate) -> Bool {
+        left.x <= right.x && left.y <= right.y
+    }
+    
+}
+
+struct Cluster {
+    let center: Coordinate
+    let coordinates: [Coordinate]
+}
