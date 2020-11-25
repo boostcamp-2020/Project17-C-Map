2befd87ed3b703c92e05a4cf1024dba0428f6089
A-by-alimelon <alimelon97@gmail.com>
2020-11-25 14:40:59 +0900

    [test]: Rule of Thumb test 함수 작성
    - 5000개 데이터 일 때 K 확인
    - 8000개 데이터 일 때 K 확인

--- Project17-C-Map/InteractiveClusteringMapTests/KDefineTests.swift
+++ Project17-C-Map/InteractiveClusteringMapTests/KDefineTests.swift
@@ -7,8 +7,18 @@
 
 import XCTest
 
-class KDefineTests: XCTestCase {
+    class KDefineTests: XCTestCase {
 
+    func test_RuleOfThumb_with_5000() throws {
+        let expected = 50
+        let numberOfData = 5000
+        XCTAssertEqual(KwithRuleOfThumb(numberOfData: numberOfData), expected)
+    }
     
+    func test_RuleOfThumb_with_8000() throws {
+        let expected = 63
+        let numberOfData = 8000
+        XCTAssertEqual(KwithRuleOfThumb(numberOfData: numberOfData), expected)
+    }
 
 }
