b583cffde45f179776fe2dbcc16240b5a7648a5f
A-by-alimelon <alimelon97@gmail.com>
2020-11-25 18:19:55 +0900

    [feat] AverageSilhouetteCalculator 클래스 생성
    각 점에서의 클러스터 내의 응집도를 구하는 findCohesion 함수 구현
    각 점에서의 실루엣 값을 구하는 findSilhouette 함수 구현

--- Project17-C-Map/InteractiveClusteringMap/KCoefficient.swift
+++ Project17-C-Map/InteractiveClusteringMap/KCoefficient.swift
@@ -12,3 +12,35 @@ func KwithRuleOfThumb(numberOfData: Int) -> Int {
     return Int(sqrt(Double(numberOfData/2)))
 }
 
+class AverageSilhouetteCalculator {
+    
+    // AverageSilhouetteMethod
+    func findAverageSilhouette(with clusters: [Cluster]) {
+        clusters.forEach { cluster in
+           
+        }
+        
+    }
+
+    func findSilhoutte(with cluster: Cluster, target: Coordinate) -> Double {
+        guard !cluster.coordinates.isEmpty else {
+            return 0.0
+        }
+        let cohesion = findCohesion(in: cluster, target: target)
+        let separation = findSeparation(in: cluster, target: target)
+        return (separation - cohesion) / max(cohesion, separation)
+    }
+
+    func findCohesion(in cluster: Cluster, target: Coordinate) -> Double {
+        var totalDistance = 0.0
+        cluster.coordinates.forEach {
+            totalDistance += target.distanceTo($0)
+        }
+        return totalDistance / Double(cluster.coordinates.count)
+    }
+
+    func findSeparation(in cluster: Cluster, target: Coordinate) -> Double {
+        var totalDistances: [Double] = []()
+    }
+}
+
