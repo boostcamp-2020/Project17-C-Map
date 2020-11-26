3f82621ad80d94a7f2fe83a4b04e5008ff9d7821
seungeon <seungeon1221@gmail.com>
2020-11-27 02:20:09 +0900

    [feat] 화면을 분할하여 중심값 생성 함수 추가

--- Project17-C-Map/InteractiveClusteringMap/Clustering/KMeans/KMeansClustering.swift
+++ Project17-C-Map/InteractiveClusteringMap/Clustering/KMeans/KMeansClustering.swift
@@ -28,6 +28,46 @@ class KMeansClustering {
         return centroids
     }
     
+    func screenCentroids(topLeft: Coordinate, bottomRight: Coordinate) -> [Coordinate] {
+        let center = (topLeft + bottomRight) / 2.0
+        let boundary = Coordinate(x: bottomRight.x - center.x, y: topLeft.y - center.y)
+        let pivot = center.findTheta(vertex: Coordinate(x: bottomRight.x, y: topLeft.y))
+        
+        let increase = Degree.turn / Double(number)
+        var angle = increase
+        var coords: [Coordinate] = []
+        
+        for _ in 0..<number {
+            let quadrant = Quadrant.findQuadrant(angle: angle)
+            let modulus = angle.truncatingRemainder(dividingBy: Degree.right)
+            
+            if modulus == 0 {
+                let distance = boundary / 2
+                let coord =  quadrant.degree(center: center, boundary: distance)
+                coords.append(coord)
+            } else {
+                let theta = quadrant.theta(angle: modulus)
+                let radian = theta * .pi / Degree.straight
+                var x: Double
+                var y: Double
+
+                if theta > pivot {
+                    y = boundary.y
+                    x = y / tan(radian)
+                } else {
+                    x = boundary.x
+                    y = tan(radian) * x
+                }
+                let distance = Coordinate(x: x, y: y) / 2
+                let coord = quadrant.convertToCoordinate(center: center, distance: distance)
+                coords.append(coord)
+            }
+            angle += increase
+        }
+        
+        return coords
+    }
+    
     /// points에 대한 centroid를 계속 계산하여 centroid가 이동한 총 거리가
     /// convergeDistance 이하로 움직일 경우 cluster를 반환합니다.
     ///
