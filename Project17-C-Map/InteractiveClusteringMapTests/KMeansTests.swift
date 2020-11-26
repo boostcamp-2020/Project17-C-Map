d1cd56cfc112becbc0584bf42a44fb4cc47d52f2
Oh-Donggeon <donggeon.dev@gmail.com>
2020-11-27 00:05:58 +0900

    [test] KMeans 클러스터링 테스트 추가
    KMeans의 클러스터링이 잘 동작하는지 Mock 데이터를 넣어 테스트 구현

--- Project17-C-Map/InteractiveClusteringMapTests/KMeansTests.swift
+++ Project17-C-Map/InteractiveClusteringMapTests/KMeansTests.swift
@@ -9,49 +9,79 @@ import XCTest
 
 class KMeansTests: XCTestCase {
     
+    var points: [Coordinate] = []
     var coordinates: [Coordinate] = []
     
     override func setUpWithError() throws {
-        coordinates = [Coordinate(x: 0, y: 2),
-        Coordinate(x: -4, y: 4),
-        Coordinate(x: 6, y: -6),
-        Coordinate(x: 8, y: 20),
-        Coordinate(x: 13, y: 54)]
+        points = [Coordinate(x: 1, y: 1),
+                  Coordinate(x: 2, y: 2),
+                  Coordinate(x: 8, y: 2),
+                  Coordinate(x: 9, y: 1),
+                  Coordinate(x: 9, y: 9),
+                  Coordinate(x: 8, y: 8),
+                  Coordinate(x: 2, y: 8),
+                  Coordinate(x: 1, y: 9)]
+        
+        coordinates = [Coordinate(x: 0, y: 0),
+                       Coordinate(x: 10, y: 10),
+                       Coordinate(x: 10, y: 0),
+                       Coordinate(x: 0, y: 10)]
     }
-
+    
     override func tearDownWithError() throws {
+        points = []
         coordinates = []
     }
     
     func test_cluster_equal() throws {
-        let cluster1 = Cluster(center: Coordinate(x: 0, y: 0), coordinates: coordinates)
-        let cluster2 = Cluster(center: Coordinate(x: 0, y: 0), coordinates: coordinates)
+        let cluster1 = Cluster(coordinates: coordinates)
+        let cluster2 = Cluster(coordinates: coordinates)
         
         XCTAssertEqual(cluster1, cluster2)
     }
     
     func test_cluster_not_equal() throws {
-        let cluster1 = Cluster(center: Coordinate(x: 0, y: 0), coordinates: coordinates)
-        let cluster2 = Cluster(center: Coordinate(x: 0, y: 1), coordinates: coordinates)
+        let cluster1 = Cluster(coordinates: coordinates)
+        coordinates.append(Coordinate(x: 10, y: 20))
+        let cluster2 = Cluster(coordinates: coordinates)
         
         XCTAssertNotEqual(cluster1, cluster2)
     }
     
     func test_cluster_equal_hash_value() throws {
-        let cluster1 = Cluster(center: Coordinate(x: 0, y: 0), coordinates: coordinates)
-        let cluster2 = Cluster(center: Coordinate(x: 3, y: 3), coordinates: coordinates)
+        let cluster1 = Cluster(coordinates: coordinates)
+        let cluster2 = Cluster(coordinates: coordinates)
         
         XCTAssertEqual(cluster1.hashValue, cluster2.hashValue)
     }
-
+    
     func test_cluster_not_equal_hash_value() throws {
         var coordinates = self.coordinates
-        let cluster1 = Cluster(center: Coordinate(x: 0, y: 0), coordinates: coordinates)
+        let cluster1 = Cluster(coordinates: coordinates)
         coordinates.append(Coordinate(x: 3, y: 17))
-        let cluster2 = Cluster(center: Coordinate(x: 0, y: 0), coordinates: coordinates)
+        let cluster2 = Cluster(coordinates: coordinates)
         
         XCTAssertNotEqual(cluster1.hashValue, cluster2.hashValue)
         XCTAssertNotEqual(cluster1, cluster2)
+    }
+    
+    func test_kmeans_clustering() throws {
+        let kmeans = KMeansClustering(k: 4)
+        let clusters = kmeans.trainCenters(points, initialCentroids: coordinates)
+        let expectPoints: [[Coordinate]] = [[Coordinate(x: 1, y: 1), Coordinate(x: 2, y: 2)],
+                                                     [Coordinate(x: 8, y: 2), Coordinate(x: 9, y: 1)],
+                                                     [Coordinate(x: 9, y: 9), Coordinate(x: 8, y: 8)],
+                                                     [Coordinate(x: 2, y: 8), Coordinate(x: 1, y: 9)]]
+        var points: [[Coordinate]] = []
+        clusters.forEach {
+            points.append($0.coordinates)
+        }
+        
+        let expected = expectPoints.contains { expectPoint in
+            points.contains { $0 == expectPoint }
+        }
+        
+        XCTAssertTrue(expected)
     }
     
 }
