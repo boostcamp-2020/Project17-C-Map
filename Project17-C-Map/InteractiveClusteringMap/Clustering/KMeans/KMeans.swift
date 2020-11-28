822d0bf19577a258e3eb9a36a6fb04e9e120922a
seungeon <seungeon1221@gmail.com>
2020-11-28 18:00:29 +0900

    [refactor] Kmeans 구조 변경
    - 중심값 생성 함수들을 프로토콜로 분리후 주입하여 사용할 수 있도록 변경

--- Project17-C-Map/InteractiveClusteringMap/Clustering/KMeans/KMeans.swift
+++ Project17-C-Map/InteractiveClusteringMap/Clustering/KMeans/KMeans.swift
@@ -7,126 +7,18 @@
 
 import Foundation
 
+protocol CentroidCreatable: AnyObject {
+    func centroids() -> [Coordinate]
+}
+
 final class KMeans {
     
-    private let k: Int
+    let k: Int
+    let centroidable: CentroidCreatable
     
-    init(k: Int) {
+    init(k: Int, centroidable: CentroidCreatable) {
         self.k = k
-    }
-    
-    func initializeBallCutCentroids(k: Int,
-                                     coverage: Double,
-                                     coordinates: [Coordinate]) -> [Coordinate] {
-        var coordinates = coordinates
-        var centroids: [Coordinate] = []
-        let container = createContainer(k: k, coordinates: coordinates)
-        centroids += pickCentroids(k: k, distance: coverage, container: container)
-        
-        if centroids.count != k {
-            for coordinate in container {
-                guard let index = coordinates.firstIndex(of: coordinate) else {
-                    return []
-                }
-                coordinates.remove(at: index)
-            }
-            
-            centroids += initializeBallCutCentroids(k: k - centroids.count + 1, coverage: coverage, coordinates: coordinates)
-        }
-        
-        return centroids
-    }
-    
-    private func createContainer(k: Int, coordinates: [Coordinate], mutiplier: Int = 5) -> [Coordinate] {
-        var coordinates = coordinates
-        var container: [Coordinate] = []
-        let count = k * mutiplier
-        for _ in 0..<count {
-            guard let coordinate = coordinates.randomElement(),
-                  let index = coordinates.firstIndex(of: coordinate)
-            else {
-                return []
-            }
-            container.append(coordinate)
-            coordinates.remove(at: index)
-        }
-        
-        return container
-    }
-    
-    private func pickCentroids(k: Int, distance: Double, container: [Coordinate]) -> [Coordinate] {
-        var centroids: [Coordinate] = []
-        var container = container
-        
-        for _ in 0..<k {
-            guard let centroid = container.randomElement() else {
-                return centroids
-            }
-            
-            var remove: [Coordinate] = []
-            
-            container.forEach { coordinate in
-                if centroid.distanceTo(coordinate) <= distance {
-                    remove.append(coordinate)
-                }
-            }
-            
-            container = container.filter { !remove.contains($0) }
-            centroids.append(centroid)
-        }
-        
-        return centroids
-    }
-    
-    func initializeRandomCentroids(rangeOfLat lats: ClosedRange<Double>,
-                         rangeOfLng lngs: ClosedRange<Double>) -> [Coordinate] {
-        var centroids: [Coordinate] = []
-        for _ in 0..<k {
-            let cluster = Coordinate.randomGenerate(rangeOfLat: lats, rangeOfLng: lngs)
-            centroids.append(cluster)
-        }
-        
-        return centroids
-    }
-    
-    func initializeScreenCentroids(topLeft: Coordinate, bottomRight: Coordinate) -> [Coordinate] {
-        let center = (topLeft + bottomRight) / 2.0
-        let boundary = Coordinate(x: bottomRight.x - center.x, y: topLeft.y - center.y)
-        let pivot = center.findTheta(vertex: Coordinate(x: bottomRight.x, y: topLeft.y))
-        
-        let increase = Degree.turn / Double(k)
-        var angle = increase
-        var coords: [Coordinate] = []
-        
-        for _ in 0..<k {
-            let quadrant = Quadrant.findQuadrant(angle: angle)
-            let modulus = angle.truncatingRemainder(dividingBy: Degree.right)
-            
-            if modulus == 0 {
-                let distance = boundary / 2
-                let coord =  quadrant.degree(center: center, boundary: distance)
-                coords.append(coord)
-            } else {
-                let theta = quadrant.theta(angle: modulus)
-                let radian = theta * .pi / Degree.straight
-                var x: Double
-                var y: Double
-                
-                if theta > pivot {
-                    y = boundary.y
-                    x = y / tan(radian)
-                } else {
-                    x = boundary.x
-                    y = tan(radian) * x
-                }
-                let distance = Coordinate(x: x, y: y) / 2
-                let coord = quadrant.convertToCoordinate(center: center, distance: distance)
-                coords.append(coord)
-            }
-            angle += increase
-        }
-        
-        return coords
+        self.centroidable = centroidable
     }
     
     /// points에 대한 centroid를 계속 계산하여 centroid가 이동한 총 거리가
@@ -137,7 +29,8 @@ final class KMeans {
     ///   - initialCentroids: 초기 중심 값
     ///   - convergeDistance: 바뀐 Clusters가 움직인 총 거리가 convergeDistance 보다 작을경우 Cluster 반환
     /// - Returns: 중심이 되는 클러스터를 반환합니다.
-    func trainCenters(_ points: [Coordinate], initialCentroids: [Coordinate], convergeDistance: Double) -> [Cluster] {
+    func trainCenters(_ points: [Coordinate], convergeDistance: Double) -> [Cluster] {
+        let initialCentroids = centroidable.centroids()
         var clusters: [Cluster]
         var beforeCenters = initialCentroids
         var totalMoveDist = Double.zero
@@ -164,7 +57,8 @@ final class KMeans {
     ///   - points: 모든 좌표 값
     ///   - initialCentroids: 초기 중심 값
     /// - Returns: 중심이 되는 클러스터를 반환합니다.
-    func trainCenters(_ points: [Coordinate], initialCentroids: [Coordinate]) -> [Cluster] {
+    func trainCenters(_ points: [Coordinate]) -> [Cluster] {
+        let initialCentroids: [Coordinate] = centroidable.centroids()
         // 초기화 한 센터에 대한 points를 classification 해준다.
         var clusters = classify(points, from: initialCentroids)
         var isChanged = true
