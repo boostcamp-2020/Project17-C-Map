958379fc1c001a40fa6a788869d6934037eaba0e
Oh-Donggeon <donggeon.dev@gmail.com>
2020-11-30 18:46:57 +0900

    [test] KMeans 테스트 코드 작성
    KMeans 구조 변경으로 인한 전체적인 테스트 코드 작성

--- Project17-C-Map/InteractiveClusteringMapTests/KMeans/KMeansTests.swift
+++ Project17-C-Map/InteractiveClusteringMapTests/KMeans/KMeansTests.swift
@@ -12,6 +12,26 @@ class KMeansTests: XCTestCase {
     var points: [Coordinate] = []
     var coordinates: [Coordinate] = []
     
+    class CentroidGenerator: CentroidGeneratable {
+        
+        private let k: Int
+        
+        private let topLeft: Coordinate
+        private let bottomRight: Coordinate
+        var coordinates: [Coordinate] = []
+        
+        init(k: Int, topLeft: Coordinate, bottomRight: Coordinate) {
+            self.k = k
+            self.topLeft = topLeft
+            self.bottomRight = bottomRight
+        }
+        
+        func centroids() -> [Coordinate] {
+            return coordinates
+        }
+        
+    }
+    
     override func setUpWithError() throws {
         points = [Coordinate(x: 1, y: 1),
                   Coordinate(x: 2, y: 2),
@@ -66,22 +86,29 @@ class KMeansTests: XCTestCase {
     }
     
     func test_kmeans_clustering() throws {
-        let kmeans = KMeans(k: 4)
-        let clusters = kmeans.trainCenters(points, initialCentroids: coordinates)
+        let centroids = ScreenCentroidGenerator(k: 4, topLeft: Coordinate(x: 0, y: 10), bottomRight: Coordinate(x: 10, y: 0))
+        let kmeans = KMeans(k: 4, centroidable: centroids, option: .state)
         let expectPoints: [[Coordinate]] = [[Coordinate(x: 1, y: 1), Coordinate(x: 2, y: 2)],
-                                                     [Coordinate(x: 8, y: 2), Coordinate(x: 9, y: 1)],
-                                                     [Coordinate(x: 9, y: 9), Coordinate(x: 8, y: 8)],
-                                                     [Coordinate(x: 2, y: 8), Coordinate(x: 1, y: 9)]]
-        var points: [[Coordinate]] = []
-        clusters.forEach {
-            points.append($0.coordinates)
+                                            [Coordinate(x: 8, y: 2), Coordinate(x: 9, y: 1)],
+                                            [Coordinate(x: 9, y: 9), Coordinate(x: 8, y: 8)],
+                                            [Coordinate(x: 2, y: 8), Coordinate(x: 1, y: 9)]]
+        var actualPoints: [[Coordinate]] = []
+        
+        kmeans.start(coordinate: self.points) {
+            $0.forEach {
+                actualPoints.append($0.coordinates)
+            }
+            
+            let expected = expectPoints.contains { expectPoint in
+                actualPoints.contains { $0 == expectPoint }
+            }
+            XCTAssertTrue(expected)
         }
         
-        let expected = expectPoints.contains { expectPoint in
-            points.contains { $0 == expectPoint }
+        waitForExpectations(timeout: 5) { error in
+            guard let error = error else { return }
+            XCTFail("timeout error: \(error)")
         }
-        
-        XCTAssertTrue(expected)
     }
     
 }
