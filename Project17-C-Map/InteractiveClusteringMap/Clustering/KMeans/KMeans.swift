b90ea717e5ad0c8cf155cc5e2fc49bd122e01eeb
seungeon <seungeon1221@gmail.com>
2020-11-30 16:02:46 +0900

    [refactor] class를 struct로 변경

--- Project17-C-Map/InteractiveClusteringMap/Clustering/KMeans/KMeans.swift
+++ Project17-C-Map/InteractiveClusteringMap/Clustering/KMeans/KMeans.swift
@@ -7,41 +7,32 @@
 
 import Foundation
 
-protocol CentroidCreatable: AnyObject {
+protocol CentroidGeneratable: AnyObject {
     func centroids() -> [Coordinate]
 }
 
-final class KMeans: ClusteringServicing {
-
+struct KMeans {
+    
     private let k: Int
-    private let centroidable: CentroidCreatable
+    private let centroidable: CentroidGeneratable
     private let option: CompleteOption
     private let coverage: Double = 1.0
-
-    init(k: Int, centroidable: CentroidCreatable, option: CompleteOption) {
+    
+    init(k: Int, centroidable: CentroidGeneratable, option: CompleteOption) {
         self.k = k
         self.centroidable = centroidable
         self.option = option
     }
     
-    func execute(coordinates: [Coordinate]?,
-                 boundingBox: BoundingBox,
-                 zoomLevel: Double,
-                 completionHandler: @escaping (([Cluster]) -> Void)) {
-        let centroids = centroidable.centroids()
-        
+    func start(coordinate: [Coordinate], completion: @escaping ([Cluster]) -> Void) {
         switch option {
         case .distance:
-            completionHandler(trainCenters(centroids, convergeDistance: coverage))
+            completion(trainCenters(coordinate, convergeDistance: coverage))
         case .state:
-            completionHandler(trainCenters(centroids))
+            completion(trainCenters(coordinate))
         }
     }
     
-    func cancel() {
-        
-    }
-
     /// points에 대한 centroid를 계속 계산하여 centroid가 이동한 총 거리가
     /// convergeDistance 이하로 움직일 경우 cluster를 반환합니다.
     ///
@@ -55,23 +46,23 @@ final class KMeans: ClusteringServicing {
         var clusters: [Cluster]
         var beforeCenters = initialCentroids
         var totalMoveDist = Double.zero
-
+        
         repeat {
             clusters = classify(points, from: beforeCenters)
             totalMoveDist = Double.zero
-
+            
             let movedCenters = clusters.map { $0.center }
-
+            
             for (index, center) in beforeCenters.enumerated() {
                 totalMoveDist += center.distanceTo(movedCenters[index])
             }
             beforeCenters = movedCenters
-
+            
         } while totalMoveDist < convergeDistance
-
+        
         return clusters
     }
-
+    
     /// points에 대한 centroid를 계속 계산하여 points의 classification에 변화가 없을 시 cluster를 반환합니다.
     ///
     /// - Parameters:
@@ -80,24 +71,23 @@ final class KMeans: ClusteringServicing {
     /// - Returns: 중심이 되는 클러스터를 반환합니다.
     private func trainCenters(_ points: [Coordinate]) -> [Cluster] {
         let initialCentroids: [Coordinate] = centroidable.centroids()
-        // 초기화 한 센터에 대한 points를 classification 해준다.
         var clusters = classify(points, from: initialCentroids)
         var isChanged = true
-
+        
         repeat {
             let centers = clusters.map { $0.center }
             let movedClusters = classify(points, from: centers)
-
+            
             if clusters.hashValue == movedClusters.hashValue {
                 isChanged = false
             }
             clusters = movedClusters
-
+            
         } while isChanged
-
+        
         return clusters
     }
-
+    
     /// 각 points에 대해 가장 가까운 center로 points를 분류하여 cluster 객체를 반환합니다.
     ///
     /// - Parameters:
@@ -106,16 +96,16 @@ final class KMeans: ClusteringServicing {
     /// - Returns: 분류한 points를 가지는 clusters를 리턴한다. (Cluster)
     private func classify(_ points: [Coordinate], from centers: [Coordinate]) -> [Cluster] {
         var clusters = [Cluster](repeating: Cluster(coordinates: []), count: centers.count)
-
+        
         points.forEach { point in
             let centerIndex = indexOfNearestCenter(point, centers: centers)
-
+            
             clusters[centerIndex].coordinates.append(point)
         }
-
+        
         return clusters
     }
-
+    
     /// point 와 센터들 중 가장 가까운 센터의 index를 반환합니다.
     ///
     /// - Parameters:
@@ -125,7 +115,7 @@ final class KMeans: ClusteringServicing {
     private func indexOfNearestCenter(_ point: Coordinate, centers: [Coordinate]) -> Int {
         var nearestDist = Double.greatestFiniteMagnitude
         var minIndex = 0
-
+        
         for (idx, center) in centers.enumerated() {
             let dist = point.distanceTo(center)
             guard dist < nearestDist else {
@@ -134,10 +124,10 @@ final class KMeans: ClusteringServicing {
             minIndex = idx
             nearestDist = dist
         }
-
+        
         return minIndex
     }
-
+    
 }
 
 extension KMeans {
