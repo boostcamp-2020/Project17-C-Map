09e0f3244afd15bf9384cbdd936177069351b8e4
seungeon <seungeon1221@gmail.com>
2020-11-30 16:14:57 +0900

    [feat] 실루엣을 이용한 KMeans 검증 레이어 클래스 추가

---
+++ Project17-C-Map/InteractiveClusteringMap/Clustering/KMeans/KMeansValidateService.swift
@@ -0,0 +1,69 @@
+//
+//  KMeansValidateService.swift
+//  InteractiveClusteringMap
+//
+//  Created by Seungeon Kim on 2020/11/29.
+//
+
+import Foundation
+
+class KMeansValidateService: ClusteringServicing {
+    
+    private let dispatchGroup = DispatchGroup()
+    private let concurrentQueue = DispatchQueue.init(label: concurrentQueueName, attributes: .concurrent)
+    private let serialQueue = DispatchQueue.init(label: serialQueueName)
+    private var bestCluster = Clusters(items: [])
+    
+    func execute(coordinates: [Coordinate]?,
+                 boundingBox: BoundingBox,
+                 zoomLevel: Double,
+                 completionHandler: @escaping (([Cluster]) -> Void)) {
+        
+        guard let coordinates = coordinates else { return }
+        
+        validateKMeans(coordinates: coordinates)
+        
+        dispatchGroup.notify(queue: serialQueue) { [weak self] in
+            guard let self = self else { return }
+
+            completionHandler(self.bestCluster.items)
+        }
+        
+    }
+    
+    func cancel() {
+        
+    }
+    
+    private func validateKMeans(coordinates: [Coordinate]) {
+        (2...10).forEach { k in
+            self.startKMeans(k: k, coordinates: coordinates)
+        }
+    }
+    
+    private func startKMeans(k: Int, coordinates: [Coordinate]) {
+        concurrentQueue.async(group: dispatchGroup) {
+            let centroidGenerator = BallCutCentroidGenerator(k: k,
+                                                             coverage: 0.001,
+                                                             coordinates: coordinates)
+            let kmeans = KMeans(k: k, centroidable: centroidGenerator, option: .state)
+            kmeans.start(coordinate: coordinates) { result in
+                self.compareSilhouette(cluster: result)
+            }
+        }
+    }
+    
+    private func compareSilhouette(cluster: [Cluster]) {
+        let cluster = Clusters(items: cluster)
+        
+        serialQueue.async {
+            self.bestCluster = self.bestCluster.silhouette > cluster.silhouette ? self.bestCluster : cluster
+        }
+    }
+    
+}
+
+extension KMeansValidateService {
+    static let concurrentQueueName = "KMeansValidateService.concurrent"
+    static let serialQueueName = "KMeansValidateService.serial"
+}
