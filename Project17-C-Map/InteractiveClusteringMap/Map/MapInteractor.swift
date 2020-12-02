ad6ae3e2a12c8afaa6db4e9c1bd6347a5827c21d
eunjeongS2 <dmswjd2340@gmail.com>
2020-12-02 22:27:17 +0900

    [refactor] clusteringServicing -> clusteringService 로 이름 변경 	 	클러스터링 병렬 큐에서 처리 제거
     클러스터링 병렬 큐에서 처리 제거 이유 : 어차피 클러스터링 내부에서 직렬큐에 넣음

--- Project17-C-Map/InteractiveClusteringMap/Map/MapInteractor.swift
+++ Project17-C-Map/InteractiveClusteringMap/Map/MapInteractor.swift
@@ -17,7 +17,7 @@ final class MapInteractor: ClusterBusinessLogic {
     
     private let poiService: POIServicing
     private let presenter: ClusterPresentationLogic
-    private var clusteringServicing: QuadTreeClusteringService?
+    private var clusteringService: ClusteringServicing?
     
     init(poiService: POIServicing, presenter: ClusterPresentationLogic) {
         self.poiService = poiService
@@ -32,16 +32,14 @@ final class MapInteractor: ClusterBusinessLogic {
                 let coordinates = pois.map {
                     Coordinate(x: $0.x, y: $0.y)
                 }
-                if self.clusteringServicing == nil {
-                    self.clusteringServicing = QuadTreeClusteringService(coordinates: coordinates,
+                if self.clusteringService == nil {
+                    self.clusteringService = QuadTreeClusteringService(coordinates: coordinates,
                                                                          boundingBox: BoundingBox.korea)
                 }
-                DispatchQueue.global(qos: .userInitiated).async {
-                    self.clustering(coordinates: coordinates,
-                                    tileId: tileId,
-                                    boundingBox: boundingBox,
-                                    zoomLevel: zoomLevel)
-                }
+                self.clustering(coordinates: coordinates,
+                                tileId: tileId,
+                                boundingBox: boundingBox,
+                                zoomLevel: zoomLevel)
             }
         }
     }
@@ -55,11 +53,11 @@ final class MapInteractor: ClusterBusinessLogic {
                             boundingBox: BoundingBox,
                             zoomLevel: Double) {
         
-        clusteringServicing?.execute(coordinates: coordinates,
+        clusteringService?.execute(coordinates: coordinates,
                                      boundingBox: boundingBox,
                                      zoomLevel: zoomLevel) { [weak self] clusters in
             guard let self = self else { return }
-                                                            
+                                      
             self.presenter.clustersToMarkers(tileId: tileId, clusters: clusters)
         }
     }
