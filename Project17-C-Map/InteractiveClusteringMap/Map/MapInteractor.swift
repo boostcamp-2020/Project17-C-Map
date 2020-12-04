811ac465f87f6b5b44039397685c7951a3c4ecaf
seungeon <seungeon1221@gmail.com>
2020-12-04 22:30:09 +0900

    [refac] clustering service class 수정으로 인한 변경
    - clustering service 생성시 poi service를 inject 해준다.

--- Project17-C-Map/InteractiveClusteringMap/Map/MapInteractor.swift
+++ Project17-C-Map/InteractiveClusteringMap/Map/MapInteractor.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 protocol ClusterBusinessLogic: class {
-
+    
     func fetch(boundingBoxes: [CLong: BoundingBox], zoomLevel: Double)
     func remove(tileIds: [CLong])
-
+    
 }
 
 final class MapInteractor: ClusterBusinessLogic {
-
+    
     private let presenter: ClusterPresentationLogic
     private let quadTreeClusteringService: ClusteringServicing
     
@@ -23,7 +23,7 @@ final class MapInteractor: ClusterBusinessLogic {
         self.presenter = presenter
         self.quadTreeClusteringService = QuadTreeClusteringService(poiService: poiService)
     }
-
+    
     func fetch(boundingBoxes: [CLong: BoundingBox], zoomLevel: Double) {
         boundingBoxes.forEach { tileId, boundingBox in
             self.clustering(
@@ -32,31 +32,27 @@ final class MapInteractor: ClusterBusinessLogic {
                 zoomLevel: zoomLevel)
         }
     }
-
+    
     func remove(tileIds: [CLong]) {
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
             self.presenter.removePresentMarkers(tileIds: tileIds)
         }
     }
-
+    
     private func clustering(tileId: CLong,
                             boundingBox: BoundingBox,
                             zoomLevel: Double) {
-
+        
         quadTreeClusteringService.execute(coordinates: nil,
                                    boundingBox: boundingBox,
                                    zoomLevel: zoomLevel) { [weak self] clusters in
             guard let self = self else { return }
-
+            
             DispatchQueue.main.async {
                 self.presenter.clustersToMarkers(tileId: tileId, clusters: clusters)
             }
         }
     }
-
-    func remove(tileIds: [CLong]) {
-        presenter.removePresentMarkers(tileIds: tileIds)
-    }
-
+    
 }
