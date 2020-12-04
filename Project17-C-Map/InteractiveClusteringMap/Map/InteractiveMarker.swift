a8cc8fede0e638aae604a79e2d2d2b3a76778918
A-by-alimelon <alimelon97@gmail.com>
2020-12-04 22:29:34 +0900

    [feat] marker 터치 핸들러 등록

--- Project17-C-Map/InteractiveClusteringMap/Map/InteractiveMarker.swift
+++ Project17-C-Map/InteractiveClusteringMap/Map/InteractiveMarker.swift
@@ -8,16 +8,31 @@
 import Foundation
 import NMapsMap
 
-final class InteractiveMarker: NMFMarker, Markerable {
+final class InteractiveMarker: NMFMarker, Markerable, UIGestureRecognizerDelegate {
     
     required init(cluster: Cluster) {
         super.init()
         position = NMGLatLng(lat: cluster.center.y, lng: cluster.center.x)
         iconTintColor = .green
+        
+        let tapGesture = UILongPressGestureRecognizer()
+        tapGesture.delegate = self
+        
+        touchHandler = { (_) -> Bool in
+            print("마커 터치")
+            return true
+        }
+        
     }
     
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        print("꾹")
+        return true
+    }
     func remove() {
         mapView = nil
     }
+    
+    
     
 }
