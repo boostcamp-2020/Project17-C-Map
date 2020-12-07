e0af1cc520976842f054ca7150554ee3cebbf1bd
alimelon <alimelon97@gmail.com>
2020-12-08 01:30:02 +0900

    [feat] infoWindow 이미지 에셋 추가

---
+++ Project17-C-Map/InteractiveClusteringMap/Map/CustomInfoWindow.swift
@@ -0,0 +1,20 @@
+//
+//  CustomInfoWindow.swift
+//  InteractiveClusteringMap
+//
+//  Created by A on 2020/12/08.
+//
+
+import UIKit
+
+class CustomInfoWindow: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
