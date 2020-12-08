18a68b7a089f276d5d9c99dfc9f118d6fe771b6c
alimelon <alimelon97@gmail.com>
2020-12-08 20:42:45 +0900

    [refac] 리뷰 반영

--- Project17-C-Map/InteractiveClusteringMap/Map/CustomInfoWindow.swift
+++ Project17-C-Map/InteractiveClusteringMap/Map/CustomInfoWindow.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class CustomInfoWindowView: UIView {
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
+    
 }
--- Project17-C-Map/InteractiveClusteringMap/Map/CustomInfoWindowDataSource.swift
+++ Project17-C-Map/InteractiveClusteringMap/Map/CustomInfoWindowDataSource.swift
@@ -10,15 +10,19 @@ import NMapsMap
 
 class CustomInfoWindowDataSource: NSObject, NMFOverlayImageDataSource {
     
+    enum Name {
+        static let title = "title"
+        static let category = "category"
+    }
+    
     func view(with overlay: NMFOverlay) -> UIView {
         let rootView = Bundle.main.loadNibNamed("CustomInfoWindowView", owner: nil, options: nil)?.first as? CustomInfoWindowView ?? CustomInfoWindowView()
         
-        guard let infoWindow = overlay as? NMFInfoWindow else { return rootView }
+        guard let infoWindow = overlay as? NMFInfoWindow,
+              infoWindow.marker != nil else { return rootView }
         
-        guard infoWindow.marker != nil else { return rootView }
-        
-        rootView.titleLabel.text = infoWindow.marker?.userInfo["title"] as? String
-        rootView.categoryLabel.text = infoWindow.marker?.userInfo["category"] as? String
+        rootView.titleLabel.text = infoWindow.marker?.userInfo[Name.title] as? String
+        rootView.categoryLabel.text = infoWindow.marker?.userInfo[Name.category] as? String
         rootView.titleLabel.sizeToFit()
         rootView.categoryLabel.sizeToFit()
         let width = rootView.titleLabel.frame.size.width + rootView.categoryLabel.frame.size.width + 10
@@ -28,3 +32,4 @@ class CustomInfoWindowDataSource: NSObject, NMFOverlayImageDataSource {
         return rootView
     }
 }
+
