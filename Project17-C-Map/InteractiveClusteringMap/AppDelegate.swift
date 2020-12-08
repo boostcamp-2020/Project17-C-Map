505a8a82eb2cb678f5438743216d04fbfef57f02
Oh-Donggeon <donggeon.dev@gmail.com>
2020-12-08 20:33:13 +0900

    [fix] core data add, update, delete 시 데이터 저장

--- Project17-C-Map/InteractiveClusteringMap/AppDelegate.swift
+++ Project17-C-Map/InteractiveClusteringMap/AppDelegate.swift
@@ -10,30 +10,25 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     private let containerName: String = "Model"
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
 
     }
     
-    func applicationWillTerminate(_ application: UIApplication) {
-        CoreDataStack.shared.save { }
-    }
-    
     // MARK: - Core Data Stack
+    
     lazy var container: NSPersistentContainer = {
         let container = NSPersistentContainer(name: containerName)
         container.loadPersistentStores(completionHandler: { (_, error) in
@@ -43,6 +38,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
-
+    
 }
 
