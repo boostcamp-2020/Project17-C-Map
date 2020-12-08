9f5566222c92f57387c70c4b7b0d29a0b99b50de
eunjeongS2 <dmswjd2340@gmail.com>
2020-12-09 01:57:04 +0900

    [feat] Place 정보 목록 화면 레이아웃 구현

---
+++ Project17-C-Map/InteractiveClusteringMap/Map/PreviewViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PreviewViewController.swift
+//  InteractiveClusteringMap
+//
+//  Created by eunjeong lee on 2020/12/08.
+//
+
+import UIKit
+import NMapsMap
+
+class PreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
