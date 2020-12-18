6b880c01a9b9cd513761db466f98687178acd368
eunjeongS2 <dmswjd2340@gmail.com>
2020-12-19 03:04:37 +0900

    [fix] OperationQueue로 작업 취소

--- Project17-C-Map/InteractiveClusteringMap/Network/PlaceInfoService.swift
+++ Project17-C-Map/InteractiveClusteringMap/Network/PlaceInfoService.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 protocol PlaceInfoServicing: class {
+    
     func fetchImage(url: String, completion: @escaping (UIImage?) -> Void)
-    func fetchAdrress(lat: Double, lng: Double, completion: @escaping (String) -> Void)
+    func fetchAddress(lat: Double, lng: Double, completion: @escaping (String) -> Void)
+    func cancel()
+    
 }
 
 final class PlaceInfoService: PlaceInfoServicing {
+    
+    private let imageOperationQueue = OperationQueue()
+    private let addressOperationQueue = OperationQueue()
     private let imageProvider: ImageProviding
     private let geocodingNetwork: Geocodable
     private var repositroy: [Coordinate: String] = [: ]
@@ -23,24 +29,47 @@ final class PlaceInfoService: PlaceInfoServicing {
     }
     
     func fetchImage(url: String, completion: @escaping (UIImage?) -> Void) {
-        imageProvider.imageURL(from: url) { url in
-            let image = UIImage(contentsOfFile: url.path)
-            completion(image)
+        let operation = BlockOperation()
+        operation.addExecutionBlock { [weak self] in
+            guard let self = self else { return }
+            if operation.isCancelled { return }
+            
+            self.imageProvider.imageURL(from: url) { url in
+                if operation.isCancelled { return }
+                
+                let image = UIImage(contentsOfFile: url.path)
+                completion(image)
+            }
         }
+        imageOperationQueue.addOperation(operation)
     }
     
-    func fetchAdrress(lat: Double, lng: Double, completion: @escaping (String) -> Void) {
-        let coordinate = Coordinate(x: lng, y: lat)
-       
-        guard let address = repositroy[coordinate] else {
-            geocodingNetwork.address(lat: lat, lng: lng) { [weak self] result in
-                guard let self = self else { return }
-                self.repositroy[coordinate] = result
-                completion(result)
+    func fetchAddress(lat: Double, lng: Double, completion: @escaping (String) -> Void) {
+        let operation = BlockOperation()
+        operation.addExecutionBlock { [weak self] in
+            guard let self = self else { return }
+            if operation.isCancelled { return }
+            
+            let coordinate = Coordinate(x: lng, y: lat)
+           
+            guard let address = self.repositroy[coordinate] else {
+                self.geocodingNetwork.address(lat: lat, lng: lng) { [weak self] result in
+                    guard let self = self else { return }
+                    if operation.isCancelled { return }
+
+                    self.repositroy[coordinate] = result
+                    completion(result)
+                }
+                return
             }
-            return
+            completion(address)
         }
-        completion(address)
+        addressOperationQueue.addOperation(operation)
+    }
+    
+    func cancel() {
+        imageOperationQueue.cancelAllOperations()
+        addressOperationQueue.cancelAllOperations()
     }
     
 }
