6e7778a5fc8506295c98a29b234f4cc96bcb2781
eunjeongS2 <dmswjd2340@gmail.com>
2020-12-20 00:33:57 +0900

    [refac] MapInteractor 분리
    - 클러스터 담당 Interactor와, POI add, remove, fetch 하는 Interactor 분리

---
+++ Project17-C-Map/InteractiveClusteringMap/Map/POIDataInteractor.swift
@@ -0,0 +1,42 @@
+//
+//  DataInteractor.swift
+//  InteractiveClusteringMap
+//
+//  Created by eunjeong lee on 2020/12/20.
+//
+
+import Foundation
+
+protocol POIDataBusinessLogic: class {
+    
+    func add(tileId: CLong, poi: POI)
+    func remove(coordinate: Coordinate)
+    func fetch(coordinate: Coordinate) -> POIInfo?
+    
+}
+
+final class POIDataInteractor: POIDataBusinessLogic {
+    
+    private let presenter: DataPresentationLogic
+    private let treeDataStore: TreeDataStorable
+    
+    init(treeDataStore: TreeDataStorable, presenter: DataPresentationLogic) {
+        self.treeDataStore = treeDataStore
+        self.presenter = presenter
+    }
+    
+    func fetch(coordinate: Coordinate) -> POIInfo? {
+        return treeDataStore.fetch(coordinate: coordinate)
+    }
+    
+    func add(tileId: CLong, poi: POI) {
+        treeDataStore.add(poi: poi)
+        presenter.add(tileId: tileId, poi: poi)
+    }
+    
+    func remove(coordinate: Coordinate) {
+        treeDataStore.remove(coordinate: coordinate)
+        presenter.delete(coordinate: coordinate)
+    }
+    
+}
